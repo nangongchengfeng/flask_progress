339f6b3445e57013e121a27d3dae23adb8c4d546
南宫乘风 <1794748404@qq.com>
2023-02-21 22:05:41 +0800

    初始化Flask代码

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5010)
