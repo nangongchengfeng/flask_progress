84ad8e2e7680236ad20a29113e61dd4aee1dc70b
南宫乘风 <1794748404@qq.com>
2023-02-24 11:11:15 +0800

    数据库私密信息接入apollo

--- apollo_config.py
+++ apollo_config.py
@@ -12,8 +12,13 @@ client = ApolloClient(app_id="zhiliaooa", cluster="default",
                       config_server_url='http://' + os.environ.get('APOLLO_CONFIG_URL'))
 
 
-#获取账号和密码
+#获取邮箱地址的 账号和密码
 MAIL_USERNAME = client.get_value('MAIL_USERNAME')
 MAIL_PASSWORD  = client.get_value('MAIL_PASSWORD')
 # print(MAIL_USERNAME,MAIL_PASSWORD)
-# 服务
+# MySQL 账号和密码
+HOSTNAME = client.get_value('HOSTNAME')
+USERNAME = client.get_value('USERNAME')
+PASSWORD = client.get_value('PASSWORD')
+DATABASE = client.get_value('DATABASE')
+
--- config.py
+++ config.py
@@ -5,18 +5,18 @@
 # @File    : config.py
 # @Software: PyCharm
 # MySQL所在的主机名
-from apollo_config import MAIL_USERNAME, MAIL_PASSWORD
+from apollo_config import MAIL_USERNAME, MAIL_PASSWORD,HOSTNAME,USERNAME,PASSWORD,DATABASE
 
-HOSTNAME = "192.168.102.20"
+HOSTNAME = HOSTNAME
 # MySQL监听的端口号，默认3306
 PORT = 3306
 # 连接MySQL的用户名，读者用自己设置的
-USERNAME = "root"
+USERNAME = USERNAME
 # 连接MySQL的密码，读者用自己的
-PASSWORD = "123456"
+PASSWORD =PASSWORD
 # MySQL上创建的数据库名称
-DATABASE = "zhiliaooa"
-
+DATABASE = DATABASE
+print("调用MySQL地址")
 DB_URI = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4"
 SQLALCHEMY_DATABASE_URI=DB_URI
 
