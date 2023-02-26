158753ad3806269780eb03ef2f2942a4c53e3d5a
南宫乘风 <1794748404@qq.com>
2023-02-26 17:14:03 +0800

    发布问答 页面的渲染

--- blueprints/qa.py
+++ blueprints/qa.py
@@ -4,12 +4,18 @@
 # @Email   : 1794748404@qq.com
 # @File    : qa.py
 # @Software: PyCharm
-from flask import Blueprint
+from flask import Blueprint, request, render_template
 
 qa = Blueprint("qa", __name__, url_prefix="/")
 
 
 @qa.route("/")
-def all_list():
+def index():
     # TODO document why this method is empty
     return "success"
+
+
+@qa.route("/qa/public", methods=["GET", "POST"])
+def public_qa():
+    if request.method == "GET":
+        return render_template('public_question.html')
