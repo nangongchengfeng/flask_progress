7068cb27845c23b9c9ff185773afcc56034527d8
南宫乘风 <1794748404@qq.com>
2023-02-26 19:19:35 +0800

    发布评论模型创建

--- models.py
+++ models.py
@@ -37,3 +37,18 @@ class QuetionModel(db.Model):
 
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     author = db.relationship(UserModel, backref='questions')
+
+
+class AnswerModel(db.Model):
+    __tablename__ = 'anser'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.Text, nullable=False)
+    create_time  = db.Column(db.DateTime, default=datetime.now)
+
+    #外键
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    question_id=db.Column(db.Integer, db.ForeignKey('question.id'))
+
+    #关系
+    author = db.relationship(UserModel, backref='answers')
+    question=db.relationship(QuetionModel,backref=db.backref('answers',order_by=create_time.desc()))
